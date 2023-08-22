4951aad66b557f0f11c45e46d75363e0d7c1491a
skumar28 <skumar28@oceaneering.com>
2023-08-22 15:24:02 +0530

    Webpack with CSS / SASS files
    - Added style-loader, css-loader, sass-loader.
    - Modification in webpack config to add support for both Sass / Css
    files.
    - Creation of main.css file.
    - Importing file in typescript.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import AlertService from './app/alert.service';
 import ComponentService from './app/component.service';
 import run from './app/app';
+import './main.css'
 
 const alertService: AlertService = new AlertService();
 const componentService: ComponentService = new ComponentService();
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,21 @@ module.exports = {
         loader: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ]
   }
 };
